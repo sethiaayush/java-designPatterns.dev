849e41976ded679c1ed06a8ff14435d8f93d9684
Aayush Sethi <sethi.aayu@gmail.com>
2018-04-17 13:32:59 +0530

    Added Singleton Design Pattern

---
+++ singleton/src/main/java/singleton/EnumAirTrafficController.java
@@ -0,0 +1,36 @@
+package singleton;
+
+public enum EnumAirTrafficController {
+	/**
+	 * Enum method is recommended as the best method of making singletons in
+	 * Java.
+	 * 
+	 * One thing to remember here is, when serializing an enum, field variables
+	 * are not getting serialized. For example, if we serialize and deserialize
+	 * the SingletonEnum class, we will lose the value of the int value field.
+	 * 
+	 * Eagerly initialized Singleton is enum based Singleton. At first glance
+	 * the code looks short and simple. However, you should be aware of the
+	 * downsides including committing to implementation strategy, extending the
+	 * enum class, serializability and restrictions to coding. These are
+	 * extensively discussed in Stack Overflow:
+	 * http://programmers.stackexchange.com/questions/179386/what-are-the-
+	 * downsides-of-implementing -a-singleton-with-javas-enum
+	 */
+	INSTANCE;
+
+	private int value;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return this.getDeclaringClass().getCanonicalName() + "@" + this.hashCode();
+	}
+}
