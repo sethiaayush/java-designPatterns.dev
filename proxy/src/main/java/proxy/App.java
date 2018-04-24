26468db3c44d89460f9c786dbc75da22d4b53c29
Aayush Sethi <sethi.aayu@gmail.com>
2018-04-24 23:41:20 +0530

    rectifiedApp

--- proxy/src/main/java/proxy/App.java
+++ proxy/src/main/java/proxy/App.java
@@ -5,8 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		Internet intConn = new InternetProxy();
 		try {
-			intConn.connectTo("java2novice.com");
-			intConn.connectTo("adult-site.com");
+			intConn.connectTo("github.com");
+			intConn.connectTo("brazzers.com");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
