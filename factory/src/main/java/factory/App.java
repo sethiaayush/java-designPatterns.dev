855c41bf2d3a45b7ad8cc991252955e5ad2aa98f
Aayush Sethi <sethi.aayu@gmail.com>
2018-05-02 23:51:28 +0530

    Factory Pattern

--- factory/src/main/java/factory/App.java
+++ factory/src/main/java/factory/App.java
@@ -1,13 +1,15 @@
 package factory;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		WeaponFactory factory = WeaponFactory.factory(builder -> {
+			builder.add(WeaponType.SWORD, Sword::new);
+			builder.add(WeaponType.AXE, Axe::new);
+			builder.add(WeaponType.SPEAR, Spear::new);
+			builder.add(WeaponType.BOW, Bow::new);
+		});
+		Weapon axe = factory.create(WeaponType.AXE);
+		System.out.println(axe.toString());
+	}
 }
