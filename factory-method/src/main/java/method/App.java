a545457d504e37b5bb5efb932e2aca91931364a9
Aayush Sethi <sethi.aayu@gmail.com>
2018-05-08 20:08:08 +0530

    Factory Method Pattern with Junit

--- factory-method/src/main/java/method/App.java
+++ factory-method/src/main/java/method/App.java
@@ -1,13 +1,25 @@
 package method;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	private WeaponFactory manufacturer;
+
+	public static void main(String[] args) {
+		App app = new App(new JapaneseWeaponFactory());
+		app.manufactureWeapons();
+
+		app = new App(new FrenchWeaponFactory());
+		app.manufactureWeapons();
+	}
+
+	public App(WeaponFactory manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	private void manufactureWeapons() {
+		Weapon weapon;
+		weapon = manufacturer.manufactureWeapon(WeaponType.SPEAR);
+		System.out.println(weapon.toString());
+		weapon = manufacturer.manufactureWeapon(WeaponType.AXE);
+		System.out.println(weapon.toString());
+	}
 }
