2236dc9d95c89f47f99014d96a791f38ab700442
Aayush Sethi <sethi.aayu@gmail.com>
2018-05-22 17:38:48 +0530

    Iterator pattern with Junit

--- iterator/src/main/java/iterator/App.java
+++ iterator/src/main/java/iterator/App.java
@@ -1,13 +1,38 @@
 package iterator;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		TreasureChest chest = new TreasureChest();
+
+		ItemIterator ringIterator = chest.iterator(ItemType.RING);
+		while (ringIterator.hasNext()) {
+			LOGGER.info(ringIterator.next().toString());
+		}
+
+		LOGGER.info("----------");
+
+		ItemIterator potionIterator = chest.iterator(ItemType.POTION);
+		while (potionIterator.hasNext()) {
+			LOGGER.info(potionIterator.next().toString());
+		}
+
+		LOGGER.info("----------");
+
+		ItemIterator weaponIterator = chest.iterator(ItemType.WEAPON);
+		while (weaponIterator.hasNext()) {
+			LOGGER.info(weaponIterator.next().toString());
+		}
+
+		LOGGER.info("----------");
+
+		ItemIterator it = chest.iterator(ItemType.ANY);
+		while (it.hasNext()) {
+			LOGGER.info(it.next().toString());
+		}
+	}
 }
